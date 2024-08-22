7fc0ce88aa1546bb8ce7609acb59a6c477654bd7
marky376 <mmutuamark@gmail.com>
2024-08-22 16:46:41 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Change making module.
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given amount toatal when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_tx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_tx >= n:
+            return -1
+        if rem - sorted_coins[coin_tx] >= 0:
+            rem -= sorted_coins[coin_tx]
+            coins_count += 1
+        else:
+            coin_tx += 1
+    return coins_count
